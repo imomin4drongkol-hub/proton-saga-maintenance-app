a407402731cb7a591535f069a0cbde39b3a83d68
imomin4drongkol <imomin4drongkol@gmail.com>
2026-09-10 16:16:16 +0800

    Add GitHub Actions for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/car-service-log/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
