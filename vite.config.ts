8a3ed672e27735fa10a2548fd0ad19e68b699af0
imomin4drongkol <imomin4drongkol@gmail.com>
2026-09-10 16:25:51 +0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/car-service-log/',
+  base: '/proton-saga-maintenance-app/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
